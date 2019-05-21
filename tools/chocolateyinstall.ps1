a8121a309f902b000f0715a054f33f9699b7ebf2
innotway <wake.up.dann@gmail.com>
2019-05-21 11:38:49 +0900

    #1 add release package(1.14)

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,24 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+$ErrorActionPreference = 'Stop';
+
+$packageName= 'posfordotnet.install'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url        = 'https://download.microsoft.com/download/F/F/F/FFF64E24-E3E6-45FE-924F-FFFA01E75F71/POSforDotNet-1.14.msi'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  fileType      = 'msi'
+  url           = $url
+
+  softwareName  = 'Microsoft POS for .NET*'
+
+  checksum      = '48924C7CE766D3B4C640F3AC270603D70DFA978A0E016C35D2F20F2D19C8C14C'
+  checksumType  = 'sha256'
+  
+  silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
+  validExitCodes= @(0, 3010, 1641)
+}
+
+Install-ChocolateyPackage @packageArgs
